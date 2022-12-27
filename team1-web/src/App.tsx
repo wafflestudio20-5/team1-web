3600782ba322adb50af6da7be671b281bbce2e31
isuk <ihyungsuk@snu.ac.kr>
2022-12-27 16:12:06 +0900

    reinstall react-router-dom, update App.tsx;Router form, add Home.tsx

--- team1-web/src/App.tsx
+++ team1-web/src/App.tsx
@@ -1,29 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from "./components/Home";
 
 // TODO: eslint, prettier 설정
 // TODO: npm / yarn 통일
-function App() {
+
+function AppRoutes() {
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route index element={<Home />} />
+    </Routes>
   );
 }
 
-export default App;
+export default function App() {
+  return (
+    <BrowserRouter>
+      <AppRoutes />
+      {/* TODO: <ToastContainer /> */}
+    </BrowserRouter>
+  );
+}
