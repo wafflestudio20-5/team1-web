23f295107ed00d2a241d7188b0ff9732d4bc15e1
isuk <ihyungsuk@snu.ac.kr>
2023-01-03 03:30:06 +0900

    Update Home; left side, account

--- team1-web/src/components/Home/index.tsx
+++ team1-web/src/components/Home/index.tsx
@@ -1,11 +1,41 @@
 import styles from './index.module.scss';
+import profileImg from '../../resources/profile-image.png';
+import { useNavigate } from 'react-router-dom';
 
 export default function Home() {
+  const navigate = useNavigate();
+
   return (
-    <div className={styles.home}>
-      <div className={styles.container}>
-        <div className={styles.account}>
-          <div className={styles['my-info']}></div>
+    <div className={styles['home']}>
+      <div className={styles['container']}>
+        <div className={styles['account']}>
+          <div className={styles['my-info']}>
+            <img src={profileImg} alt='프로필 이미지' />
+            {/* TODO: 세션 정보로 고치기 */}
+            <div className={styles['nickname']}>닉네임</div>
+            <div>이름</div>
+            <div>아이디</div>
+            <ul>
+              <li>
+                <button
+                  onClick={() => {
+                    navigate('');
+                  }}
+                >
+                  내 정보
+                </button>
+              </li>
+              <li>
+                <button
+                  onClick={() => {
+                    navigate('');
+                  }}
+                >
+                  로그아웃
+                </button>
+              </li>
+            </ul>
+          </div>
           <div className={styles['my-article']}></div>
           <div className={styles['banners']}></div>
         </div>
