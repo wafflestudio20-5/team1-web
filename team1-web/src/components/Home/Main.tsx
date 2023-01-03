0911740230f56bb03dd8c919064944c331c9a289
isuk <ihyungsuk@snu.ac.kr>
2023-01-03 13:52:02 +0900

    Update lib/types

--- team1-web/src/components/Home/Main.tsx
+++ team1-web/src/components/Home/Main.tsx
@@ -3,8 +3,8 @@ import styles from './Main.module.scss';
 export default function Main() {
   return (
     <div className={styles['main']}>
-      <div className={styles['banner']}></div>
-      <div className={styles['boards']}>{/* TODO: Board */}</div>
+      <div className={styles['banner']}>배너</div>
+      <div className={styles['boards']}></div>
       <div className={styles['bookstore']}></div>
     </div>
   );
--- team1-web/src/lib/types.ts
+++ team1-web/src/lib/types.ts
@@ -3,9 +3,44 @@ export type Menu = {
   name: string;
 };
 
+export type User = {
+  id: number;
+  nickname: string;
+  name: string;
+  ID: string;
+};
+
+export type AdditionalComment = {
+  id: number;
+  content: string;
+  author: User;
+  time: string;
+  like: number;
+};
+export type Comment = {
+  id: number;
+  content: string;
+  author: User;
+  time: string;
+  like: number;
+  additionalComments: AdditionalComment[];
+};
+
+export type Article = {
+  id: number;
+  title: string;
+  author: User;
+  time: string;
+  like: number;
+  comments: Comment[];
+  scrap: number;
+};
+
 export type Board = {
   id: number;
   name: string;
+  // TODO: 데이터 수정 후 물음표 떼기
+  articles?: Article[];
 };
 
 export type BoardList = {
