650726f9a5f30087c0d39b58d8407d49fd19d90d
isuk <ihyungsuk@snu.ac.kr>
2023-01-06 03:16:38 +0900

    Update Login; Update navigate path /main => /home

--- team1-web/src/components/Login/Login.tsx
+++ team1-web/src/components/Login/Login.tsx
@@ -1,12 +1,12 @@
-import styles from "./Login.module.scss";
-import { Link, useNavigate } from "react-router-dom";
-import { useState } from "react";
-import axios from "axios";
-import { useLoginProvider } from "../../LoginContext";
+import styles from './Login.module.scss';
+import { Link, useNavigate } from 'react-router-dom';
+import { useState } from 'react';
+import axios from 'axios';
+import { useLoginProvider } from '../../LoginContext';
 
 export default function Login() {
-  const [ID, setID] = useState("");
-  const [PW, setPW] = useState("");
+  const [ID, setID] = useState('');
+  const [PW, setPW] = useState('');
 
   const { setToken, setRefreshToken, setUser } = useLoginProvider();
 
@@ -20,19 +20,19 @@ export default function Login() {
     };
     const header = {
       withCredentials: true,
-      credentials: "include",
+      credentials: 'include',
     };
     axios
-      .post("http://api.wafflytime.com/api/auth/local/login", data, header)
+      .post('http://api.wafflytime.com/api/auth/local/login', data, header)
       .then((response) => {
         // TODO: Set user data
         // setUser(response["data"].owner);
-        setToken(response["data"].access_token);
-        navigate("/main/");
+        setToken(response['data'].access_token);
+        navigate('/home');
       })
       .catch((error) => {
         console.log(error.response);
-        navigate("/main/");
+        navigate('/home/');
         if (error.response.status === 401) {
           // TODO: Error handling(Incorrect password)
           // toast.error("잘못된 로그인 정보입니다!");
@@ -44,49 +44,49 @@ export default function Login() {
   };
 
   return (
-    <div id="container" className={styles["login"]}>
-      <h1 className={styles["logo"]}>
-        <Link to="/">와플리타임</Link>
+    <div id='container' className={styles['login']}>
+      <h1 className={styles['logo']}>
+        <Link to='/'>와플리타임</Link>
       </h1>
-      <p className={styles["input"]}>
+      <p className={styles['input']}>
         <input
-          type="text"
-          name="userid"
-          className={styles["text"]}
-          placeholder="아이디"
+          type='text'
+          name='userid'
+          className={styles['text']}
+          placeholder='아이디'
           value={ID}
           onChange={(e) => setID(e.target.value)}
         />
       </p>
-      <p className={styles["input"]}>
+      <p className={styles['input']}>
         <input
-          type="password"
-          name="password"
-          className={styles["text"]}
-          placeholder="비밀번호"
+          type='password'
+          name='password'
+          className={styles['text']}
+          placeholder='비밀번호'
           value={PW}
           onChange={(e) => setPW(e.target.value)}
         />
       </p>
-      <input type="hidden" name="redirect" value="/" />
-      <p className={styles["submit"]}>
+      <input type='hidden' name='redirect' value='/' />
+      <p className={styles['submit']}>
         <input
-          type="submit"
-          value="로그인"
-          className={styles["text"]}
+          type='submit'
+          value='로그인'
+          className={styles['text']}
           onClick={login}
         />
       </p>
-      <label className={styles["autologin"]}>
-        <input type="checkbox" name="autologin" value="1" />
+      <label className={styles['autologin']}>
+        <input type='checkbox' name='autologin' value='1' />
         로그인 유지
       </label>
-      <p className={styles["find"]}>
-        <Link to="/forgot">아이디/비밀번호 찾기</Link>
+      <p className={styles['find']}>
+        <Link to='/forgot'>아이디/비밀번호 찾기</Link>
       </p>
-      <p className={styles["register"]}>
+      <p className={styles['register']}>
         <span>와플리타임에 처음이신가요?</span>
-        <Link to="/register">회원가입</Link>
+        <Link to='/register'>회원가입</Link>
       </p>
     </div>
   );
