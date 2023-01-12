6997e701f463f93c808abb514364f55fb44b5431
isuk <ihyungsuk@snu.ac.kr>
2023-01-12 17:03:36 +0900

    Kakao.tsx, LoginContext.tsx 파일 복구

--- team1-web/src/components/Login/Oauth/Kakao.tsx
+++ team1-web/src/components/Login/Oauth/Kakao.tsx
@@ -1,22 +1,78 @@
+import axios from 'axios';
 import { useEffect } from 'react';
 import { useNavigate, useSearchParams } from 'react-router-dom';
 import styles from './Kakao.module.scss';
+import { toast } from 'react-toastify';
+import { useLoginProvider } from '../../../LoginContext';
 import kakaoImg from '../../../resources/kakaotalk-seeklogo.com.svg';
-import { kakaoLogin } from '../../../store/sessionSlice';
-import { useAppDispatch } from '../../../store';
 
 export default function Kakao() {
-  const navigate = useNavigate();
-  const dispatch = useAppDispatch();
-  const [code] = useSearchParams();
+  const [code, setCode] = useSearchParams();
 
-  const handleKakaoLogin = async () => {
-    await dispatch(kakaoLogin(code));
-    navigate('/home');
+  const { setToken, setRefreshToken, setUser } = useLoginProvider();
+
+  const navigate = useNavigate();
+
+  const KakaoSignup = () => {
+    // TODO: 로그인 유지 추가
+    const header = {
+      // TODO: 추후 아래 내용 삭제
+      // withCredentials: true,
+      // credentials: "include",
+    };
+    axios
+      .post(
+        `http://api.staging.wafflytime.com/api/auth/social/signup/kakao?${code}`
+      )
+      .then((response) => {
+        // TODO: Set user data
+        // setUser(response["data"].owner);
+        setToken(response.data.accessToken);
+        navigate('/home');
+        toast.success('로그인되었습니다.');
+      })
+      .catch((error) => {
+        if (error.response.status === 401) {
+          toast.error('Kakao에서 제공한 코드가 잘못되었습니다.');
+        } else if (error.response.status === 409) {
+          toast.error('이미 가입된 계정입니다.');
+        } else {
+          toast.error('알 수 없는 에러가 발생했습니다. 운영진에게 문의하세요.');
+        }
+      });
+  };
+
+  const KakaoLogin = () => {
+    // TODO: 로그인 유지 추가
+    const header = {
+      // TODO: 추후 아래 내용 삭제
+      // withCredentials: true,
+      // credentials: "include",
+    };
+    axios
+      .post(
+        `http://api.staging.wafflytime.com/api/auth/social/login/kakao?${code}`
+      )
+      .then((response) => {
+        // TODO: Set user data
+        // setUser(response["data"].owner);
+        setToken(response.data.accessToken);
+        navigate('/home');
+        toast.success('로그인되었습니다.');
+      })
+      .catch((error) => {
+        if (error.response.status === 401) {
+          toast.error('Kakao에서 제공한 코드가 잘못되었습니다.');
+        } else if (error.response.status === 404) {
+          KakaoSignup();
+        } else {
+          toast.error('알 수 없는 에러가 발생했습니다. 운영진에게 문의하세요.');
+        }
+      });
   };
 
   useEffect(() => {
-    handleKakaoLogin();
+    KakaoLogin();
   }, []);
 
   return (
